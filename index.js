034569ef1391cbbfd45a8102d98887a2fb0a83a1
Michael Dear <michaelcdear@gmail.com>
2022-06-27 20:14:41 -0600

    commit tree 4d191c0456c0ab400d4d35b700280cee91dbceaf

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end)*264)
+}
+
+function calculatesFarePrice(start, end) {
+    if (Math.abs((start - end)*264) <= 400){
+        return 0
+    } else if (Math.abs((start - end)*264) <= 2000) {
+        return (Math.abs((start - end)*264) - 400) * 0.02
+    } else if (Math.abs((start - end)*264) <= 2500) {
+        return 25
+    } else if (Math.abs((start - end)*264) > 2500) {
+        return "cannot travel that far"
+    }
+}
